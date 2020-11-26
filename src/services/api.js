8db88c6afa828e7996b373c05022670000c23f3c
Husam Abuhajjaj <buildrealapp@gmail.com>
2020-11-26 09:45:44 +0000

    Add env and env example files

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,8 @@
 import axios from "axios";
 
-const token =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YjA0MzQ4YTlhOTIzMzY2NTg0NzUxZDY0ZTc1NDIzZiIsInN1YiI6IjVmYWFiYzcwMmEwOWJjMDA0MThlODI2ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gFWvilnXld3rm7-LN333OU-TAu-wJiJzLv-9r5w94jM";
-
 export default axios.create({
   baseURL: "https://api.themoviedb.org/3",
   headers: {
-    Authorization: `Bearer ${token}`,
+    Authorization: `Bearer ${process.env.VUE_APP_TMDB_TOKEN}`,
   },
 });
